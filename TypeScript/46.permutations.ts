964334bb5663b4a23ee4f2a961f6edfff198a550
Huan Luo <dukeluo@outlook.com>
2023-11-08 00:13:37 +0800

    feat: add another solution for Permutations

--- TypeScript/46.permutations.ts
+++ TypeScript/46.permutations.ts
@@ -19,3 +19,22 @@ function permute(nums: number[]): number[][] {
 
   return allPerms;
 }
+
+function permute2(nums: number[], collected: number[] = []): number[][] {
+  if (nums.length === 0) {
+    return [collected];
+  }
+
+  const allPerms: number[][] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const rest = nums.slice(0, i).concat(nums.slice(i + 1));
+
+    for (let perm of permute2(rest, [...collected, num])) {
+      allPerms.push(perm);
+    }
+  }
+
+  return allPerms;
+}
